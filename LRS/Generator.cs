bf5a750da6e1b4a99b85af138e0176159e9a9bcd
Efim Kubishkin <fiska2003@mail.ru>
2023-03-16 23:10:44 +0300

    minor fixes
    syntactic fix, add readonly to fields of the class, delete extra constructor, remove extra field (q)

--- LRS/Generator.cs
+++ LRS/Generator.cs
@@ -3,33 +3,18 @@ namespace LRS
 {
     public class CSR
     {
-        private int k;
-        private PolinomOverSimpleField q;
-        private BinaryFiniteField F;
-        private FiniteFieldElement[] Coeffs;
-        private FiniteFieldElement c;
-        private List<FiniteFieldElement> Sequence = new List<FiniteFieldElement>();
-        public CSR(FiniteFieldElement[] coeffs, int c, List<FiniteFieldElement> FirstElementsOfSequense)
-        {
-            if (coeffs.Length == FirstElementsOfSequense.Count)
-            {
-                k = FirstElementsOfSequense.Count;
-                Sequence.AddRange(FirstElementsOfSequense);
-                this.q = new PolinomOverSimpleField(new int[] { 1, 1, 0, 1, 1, 0, 0, 0, 1 }, 2);
-                this.F = new BinaryFiniteField(8, this.q);
-                Coeffs = new FiniteFieldElement[k];
-                coeffs.CopyTo(Coeffs, 0);
-                this.c = F.FromNumberToElement(c);
-            }
-            else throw new Exception("Неккоректные входные данные");
-        }
+        private readonly int k;
+        private readonly BinaryFiniteField F;
+        private readonly FiniteFieldElement[] Coeffs;
+        private readonly FiniteFieldElement c;
+        private readonly List<FiniteFieldElement> Sequence = new List<FiniteFieldElement>();
         public CSR(int[] coeffs, int absoluteTerm, int[] FirstElementsOfSequense)
         {
             if (coeffs.Length == FirstElementsOfSequense.Length)
             {
                 k = FirstElementsOfSequense.Length;
-                this.q = new PolinomOverSimpleField(new int[] { 1, 1, 0, 1, 1, 0, 0, 0, 1 }, 2);
-                this.F = new BinaryFiniteField(8, this.q);
+                var q = new PolinomOverSimpleField(new int[] { 1, 1, 0, 1, 1, 0, 0, 0, 1 }, 2);
+                this.F = new BinaryFiniteField(8, q);
                 Coeffs = new FiniteFieldElement[k];
                 for (int i = 0; i < k; i++)
                 {
@@ -38,7 +23,7 @@ namespace LRS
                 }
                 this.c = F.FromNumberToElement(absoluteTerm);
             }
-            else throw new Exception("Неккоректные входные данные");
+            else throw new Exception("Некорректные входные данные");
         }
         public int GetRandom()
         {
